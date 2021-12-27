c66c7d7b09a9fd72937f4e3021b6e319769756bb
Lena <elena.nazarova@skoltech.ru>
2021-12-27 14:05:32 +0300

    first

---
+++ Week1/Min_string.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string x, y, z;
+
+	cin >> x >> y >> z;
+
+	if (x < y && y < z)
+	{
+		cout << x;
+	}else if (y < x && y < z)
+	{
+		cout << y;
+	}else if (z < x && z < y)
+	{
+		cout << z;
+	}
+
+	return 0;
+}
