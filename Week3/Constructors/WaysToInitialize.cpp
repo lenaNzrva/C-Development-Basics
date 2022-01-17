aafff00e45784c1a00010d6ab186d762d3542b58
Lena <elena.nazarova@skoltech.ru>
2022-01-17 21:28:20 +0300

    Finished week 3

---
+++ Week3/Constructors/WaysToInitialize.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Incognizable
+{
+    int first = 0;
+    int second = 0;
+};
+
+
+int main()
+{
+//   Incognizable a;
+//   Incognizable b = {};
+//   Incognizable c = {0};
+//   Incognizable d = {0, 1};
+
+    return 0;
+}
