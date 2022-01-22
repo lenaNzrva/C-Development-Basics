5551e83344cab6eebdfa1219677f7da35c20f968
Lena <elena.nazarova@skoltech.ru>
2022-01-23 01:31:39 +0300

    week4 in progress

--- Week4/TextFilesAndFlow/TableReadingAndOutput.cpp
+++ Week4/TextFilesAndFlow/TableReadingAndOutput.cpp
@@ -5,23 +5,42 @@
 
 using namespace std;
 
+// void PrintM(int M, ifstream& input)
+// {
+//     int value;
+//     for (int j=0; j<M; ++j)
+//     {
+//         input >> value;
+//         input.ignore(1);
+//         cout << value;
+//         // cout << setfill('.');
+//         // cout << setw(10);
+//     }
+// }
+
 int main()
 {
     // string path = "input.txt";
-    string path = "C:/Users/lena/Desktop/Cpp_Development_Basics/need to add/TextFilesAndFlow/input3.txt";
+    string path = "C:/Users/lena/Desktop/Cpp_Development_Basics/Week4/TextFilesAndFlow/input3.txt";
     ifstream input(path);
 
     if (input)
     {
         int N, M;
         string value;
-        // input >> N;
-        // input.ignore(1);
-        // input >> M;
+        input >> N;
+        input.ignore(1);
+        input >> M;
 
-        getline(input, value);
+        for (int j=0; j+1<M; ++j)
+        {
+            getline(input, value, ',');
+            cout << value;
+        }
+        getline(input, value, '\n');
         cout << value;
-
+        
+        
 
     }else{cout<<"smth wrong.";}
 
