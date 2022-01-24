4ccacf3d876afdf530d052e130a8d0cc3053daf2
Lena <elena.nazarova@skoltech.ru>
2022-01-24 20:25:10 +0300

    week 4

--- Week4/ClassRational.cpp
+++ Week4/ClassRational.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -65,11 +67,96 @@ private:
 
 Rational operator+ (const Rational& lhs, const Rational& rhs)
   {
-      return Rational(1,2);
+      int lhsNewN, rhsNewN;
+      if (lhs.Denominator() != rhs.Denominator())
+      {
+          lhsNewN = lhs.Numerator()*rhs.Denominator();
+          rhsNewN = rhs.Numerator()*lhs.Denominator();
+          return Rational(lhsNewN+rhsNewN, rhs.Denominator()*lhs.Denominator());
+      }else{return Rational(lhs.Numerator()+rhs.Numerator(), lhs.Denominator());}
   }
 
-int main() 
+Rational operator- (const Rational& lhs, const Rational& rhs)
 {
+    int lhsNewN, rhsNewN;
+    if (lhs.Denominator() != rhs.Denominator())
+    {
+        lhsNewN = lhs.Numerator()*rhs.Denominator();
+        rhsNewN = rhs.Numerator()*lhs.Denominator();
+        return Rational(lhsNewN-rhsNewN, rhs.Denominator()*lhs.Denominator());
+    }else{return Rational(lhs.Numerator()-rhs.Numerator(), lhs.Denominator());}
+}
+
+bool operator== (const Rational& lhs, const Rational& rhs)
+{
+    int lhsNewN, rhsNewN;
+    if (lhs.Denominator() != rhs.Denominator())
+    {
+        lhsNewN = lhs.Numerator()*rhs.Denominator();
+        rhsNewN = rhs.Numerator()*lhs.Denominator();
+        return lhsNewN==rhsNewN;
+    }else{return lhs.Numerator()==rhs.Numerator();}
+}
+
+Rational operator* (const Rational& lhs, const Rational& rhs)
+{
+    return Rational(lhs.Numerator()*rhs.Numerator(), lhs.Denominator()*rhs.Denominator());
+}
+
+Rational operator/ (const Rational& lhs, const Rational& rhs)
+{
+    return Rational(lhs.Numerator()*rhs.Denominator(), lhs.Denominator()*rhs.Numerator());
+}
+
+ostream& operator<< (ostream& stream,const Rational& object)
+{
+    stream << object.Numerator()<<'/'<<object.Denominator();
+    return stream;
+}
+
+istream& operator>> (istream& stream, Rational& object)
+{
+    if (stream)
+    {
+        cout << "lena" << endl;
+        int N, D;
+        stream >> N;
+        stream.ignore(1);
+        stream >> D;
+
+        object = {N,D};
+    }
     
+    return stream;
+}
+
+
+int main() {
+
+    {
+        istringstream input("5/7 10/8");
+        Rational r1, r2;
+        input >> r1 >> r2;
+        cout <<  r1.Numerator() << " " << r1.Denominator() << endl;
+        cout << r2.Numerator() << " " << r2.Denominator() << endl;
+        bool correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
+
+        if (!correct) {
+            cout << "Multiple values are read incorrectly: " << r1 << " " << r2 << endl;
+            return 4;
+        }
+
+        input >> r1;
+        input >> r2;
+        cout <<  r1.Numerator() << " " << r1.Denominator() << endl;
+        cout << r2.Numerator() << " " << r2.Denominator() << endl;
+        correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
+        if (!correct) {
+            cout << "Read from empty stream shouldn't change arguments: " << r1 << " " << r2 << endl;
+            return 5;
+        }
+    }
+
+    cout << "OK" << endl;
     return 0;
 }
