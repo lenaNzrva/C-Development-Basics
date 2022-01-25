759673eb9f5a9882f382c2869298db4e953db445
Lena <elena.nazarova@skoltech.ru>
2022-01-25 21:23:34 +0300

    still 4 week

--- Week4/ClassRational.cpp
+++ Week4/ClassRational.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <fstream>
 #include <sstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -119,79 +119,66 @@ ostream& operator<< (ostream& stream,const Rational& object)
 
 istream& operator>> (istream& stream, Rational& object)
 {
-    string test;
-    getline(stream, test, '/');
-    cout << test << endl;
+    string t1, t2;
+    string test, test2;
+    if (stream)
+    {
+        getline(stream, t1, '/');
+        getline(stream, t2, ' ');
+
+    }
+
+    if (t1.size()!=0 && t2.size()!=0)
+    {
+        for (int i=0; i<t1.size(); ++i)
+        {
+            if (i==0 && (t1[i]=='-' || t1[i]=='+'))
+            {
+                test += t1[i];
+            }
+            else if (isdigit(t1[i]))
+            {
+                test += t1[i];
+            }
+            else
+            {
+                object;
+                return stream;
+            }
+        }
+
+        for (int i=0; i<t2.size(); ++i)
+        {
+            if (i==0 && (t2[i]=='-' || t2[i]=='+'))
+            {
+                test2 += t2[i];
+            }
+            else if (isdigit(t2[i]))
+            {
+                test2 += t2[i];
+            }
+            else
+            {
+                object;
+                return stream;
+            }
+        }
+
+        object = {stoi(test), stoi(test2)};
+    }
+
+    
+    else
+    {
+        object;
+    }
 
     return stream;
+
 }
 
 
 int main() {
-    {
-        ostringstream output;
-        output << Rational(-6, 8);
-        if (output.str() != "-3/4") {
-            cout << "Rational(-6, 8) should be written as \"-3/4\"" << endl;
-            return 1;
-        }
-    }
 
-    {
-        istringstream input("5/7");
-        Rational r;
-        input >> r;
-        bool equal = r == Rational(5, 7);
-        if (!equal) {
-            cout << "5/7 is incorrectly read as " << r << endl;
-            return 2;
-        }
-    }
-
-    {
-        istringstream input("");
-        Rational r;
-        bool correct = !(input >> r);
-        if (!correct) {
-            cout << "Read from empty stream works incorrectly" << endl;
-            return 3;
-        }
-    }
-
-    {
-        istringstream input("5/7 10/8");
-        Rational r1, r2;
-        input >> r1 >> r2;
-        bool correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
-        if (!correct) {
-            cout << "Multiple values are read incorrectly: " << r1 << " " << r2 << endl;
-            return 4;
-        }
-
-        input >> r1;
-        input >> r2;
-        correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
-        if (!correct) {
-            cout << "Read from empty stream shouldn't change arguments: " << r1 << " " << r2 << endl;
-            return 5;
-        }
-    }
-
-    {
-        istringstream input1("1*2"), input2("1/"), input3("/4");
-        Rational r1, r2, r3;
-        input1 >> r1;
-        input2 >> r2;
-        input3 >> r3;
-        bool correct = r1 == Rational() && r2 == Rational() && r3 == Rational();
-        if (!correct) {
-            cout << "Reading of incorrectly formatted rationals shouldn't change arguments: "
-                 << r1 << " " << r2 << " " << r3 << endl;
-
-            return 6;
-        }
-    }
-
-    cout << "OK" << endl;
     return 0;
 }
