9a95cab7451113a7ffb9059bc496857a367e3e44
Lena <elena.nazarova@skoltech.ru>
2022-02-21 12:53:01 +0300

    work on task tracker

---
+++ YellowBelt/Week1/IntegerTypes/Test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    uint8_t A = 10;
+    uint16_t B = 5;
+
+    cout << A*B;
+
+
+    return 0;
+}   
--- YellowBelt/Week1/TuplesAndPairs/TaskTrecker.cpp
+++ YellowBelt/Week1/TuplesAndPairs/TaskTrecker.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <tuple>
 
 using namespace std;
 
@@ -15,28 +16,65 @@ enum class TaskStatus {
 // позволяющего хранить количество задач каждого статуса
 using TasksInfo = map<TaskStatus, int>;
 
+void PrintTasksInfo(TasksInfo tasks_info) {
+  cout << tasks_info[TaskStatus::NEW] << " new tasks" <<
+      ", " << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress" <<
+      ", " << tasks_info[TaskStatus::TESTING] << " tasks are being tested" <<
+      ", " << tasks_info[TaskStatus::DONE] << " tasks are done" << endl;
+}
+
 class TeamTasks {
 public:
-  // Получить статистику по статусам задач конкретного разработчика
-  const TasksInfo& GetPersonTasksInfo(const string& person) const;
-  
-  // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
-  void AddNewTask(const string& person)
+  const TasksInfo& GetPersonTasksInfo(const string& person) const
   {
-    //   TasksInfo[]
+    return M.at(person);
   }
   
-  // Обновить статусы по данному количеству задач конкретного разработчика,
-  // подробности см. ниже
-  tuple<TasksInfo, TasksInfo> PerformPersonTasks(
-      const string& person, int task_count);
+  void AddNewTask(const string& person)
+  {
+    M[person][TaskStatus::NEW] ++;
+  }
+
+  void PerformPersonTasks(const string& person, int task_count)
+  {
+    map<TaskStatus, int> CopyForIteration = M[person];
+    for (auto &i : CopyForIteration)
+    {
+      if (i.second != 0 && task_count !=0)
+      {
+        if ((i.second-task_count)>0 || (i.second-task_count)==0)
+        {
+          M[person][i.first] -= task_count;
+          task_count = 0;
+        }else
+        {
+          M[person][i.first] -= i.second;
+          task_count -= i.second;
+        }
+      }
+    }
+  }
 
 private:
-    map<string, TasksInfo&> M;
+  map<string, TasksInfo> M;
+  // map<string, TasksInfo> CopyForIteration;
 };
 
 int main()
 {
+  TeamTasks tasks;
+  tasks.AddNewTask("Ilia");
+  for (int i = 0; i < 3; ++i) {
+    tasks.AddNewTask("Ivan");
+  }
+  cout << "Ilia's tasks: ";
+  PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
+  cout << "Ivan's tasks: ";
+  PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
+
+  tasks.PerformPersonTasks("Ivan", 2);
+  cout << "Ivan's tasks: ";
+  PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
 
     return 0;
 }
