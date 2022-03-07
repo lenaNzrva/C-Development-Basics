595026799a6d64336c877fcdb551fde5ec5df5c7
Lena <elena.nazarova@skoltech.ru>
2022-03-07 14:03:27 +0300

    finished Week1

--- YellowBelt/Week1/FunctionTemplates/LinkToTheDefaultElem.cpp
+++ YellowBelt/Week1/FunctionTemplates/LinkToTheDefaultElem.cpp
@@ -2,10 +2,19 @@
 #include <map>
 using namespace std;
 
-int& GetRefStrict(map<int, int> &M, int key);
+template <typename key, typename value>
+value& GetRefStrict(map<key, value> &M, key K)
+{
+    if (M.count(K)){return M[K];}
+    else{throw runtime_error("");}
+}
 
 int main()
 {
+    // map<int, string> m = {{0, "value"}};
+    // string& item = GetRefStrict(m, 0);
+    // item = "newvalue";
+    // cout << m[0] << endl; // выведет newvalue
 
     return 0;
 }
